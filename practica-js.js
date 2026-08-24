b557250212ed260cf764b736439c5432ea0bb607
Geidy Charid Susa Amarillo <lg.susa.b@gmail.com>
2026-08-24 10:00:53 -0500

    Se desarrolla el reto 3: arreglos y objetos

--- practica-js.js
+++ practica-js.js
@@ -59,3 +59,17 @@ const nombreCompleto = (nombre, apellido) => `${nombre} ${apellido}`;
 console.log(cuadrado(5));
 console.log(esMayorEdad(20));
 console.log(nombreCompleto("Mariana", "Pérez"));
+
+// Reto 3:
+const productos = [
+{ id: 1, nombre: 'Mouse', precio: 50000, categoria: 'Perifericos', stock: 5 },
+{ id: 2, nombre: 'Teclado', precio: 90000, categoria: 'Perifericos', stock: 0 },
+{ id: 3, nombre: 'Monitor', precio: 650000, categoria: 'Pantallas', stock: 3 },
+{ id: 4, nombre: 'Audifonos', precio: 50000, categoria: 'Perifericos', stock: 10 },
+{ id: 5, nombre: 'Mouse Pad', precio: 50000, categoria: 'Accsesorios', stock: 20 },
+{ id: 6, nombre: 'Microfono', precio: 700000, categoria: 'Perifericos', stock: 3 },
+{ id: 7, nombre: 'Camara', precio: 80000, categoria: 'Dispositivos', stock: 5 },
+{ id: 8, nombre: 'Trípode', precio: 100000, categoria: 'Accesorios', stock: 0 },
+{ id: 9, nombre: 'Consola', precio: 2000000, categoria: 'Entretenimiento', stock: 8 },
+{ id: 10, nombre: 'Power Bank', precio: 90000, categoria: 'Accesorios', stock: 7 }
+];
